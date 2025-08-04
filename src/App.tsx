a539b69fab50a14d19430279529e2d6dacc37441
Charles Gonnaud <uyo@hotmail.fr>
2025-08-05 00:06:26 +0200

    Working version with task checks

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,51 @@ import MenuSvg from "./assets/icons/menu.svg?react"
 import ShareSvg from "./assets/icons/share.svg?react"
 import "./App.css"
 
-class Task {
-  constructor(
-    public id: string,
-    public name: string,
-    public quantity: string,
-    public store: string,
-    public checked: boolean,
-  ) {}
+interface Task {
+  id: string
+  name: string
+  quantity: string
+  store: string
+  checked: boolean
 }
 
-const task1 = new Task("1", "task1", "100g", "Rayon1", false)
-const task2 = new Task("2", "task2", "100g", "Rayon2", true)
-const tasks = [task1, task2]
-
-const headerColor = "#4caf50"
-
-function TaskItem({ name }: { name: string }) {
-  return <p>{name} </p>
+function TaskItem({ task, onTaskUpdated }: { task: Task; onTaskUpdated: any }) {
+  return (
+    <div
+      onClick={() => {
+        onTaskUpdated({ ...task, checked: !task.checked })
+      }}
+      style={{ display: "flex", flexDirection: "row" }}
+    >
+      <input
+        style={{ margin: "10px" }}
+        type="checkbox"
+        readOnly
+        checked={task.checked}
+      />
+      <p>{task.name}</p>
+      <p style={{ color: "gold", marginLeft: "10px" }}>{task.quantity}</p>
+    </div>
+  )
 }
 
-function Header() {
+function Header({ tasks }: { tasks: Task[] }) {
   const nbCompleted = tasks.filter((t) => t.checked).length
   const percent = Math.floor((100 * nbCompleted) / tasks.length)
   return (
     <header
+      className="header"
       style={{
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "flex-start",
-        backgroundColor: headerColor,
         gap: "15px",
         width: "100%",
       }}
     >
       <MenuSvg width="32px" height="32px" />
-      <div style={{ fontSize: "18px", fontWeight: "bold" }}>Mes Courses</div>
+      <h2>Mes Courses</h2>
       <div>
         {" "}
         {nbCompleted}/{tasks.length} ({percent}%)
@@ -50,27 +58,34 @@ function Header() {
   )
 }
 
-function Section({ title, tasks }: { title: string; tasks: Task[] }) {
+function Section({
+  title,
+  tasks,
+  onTaskUpdated,
+}: {
+  title: string
+  tasks: Task[]
+  onTaskUpdated: any
+}) {
   const [show, setShow] = useState(true)
   return (
     <div
       style={{
         display: "flex",
-        backgroundColor: "grey",
         flexDirection: "column",
       }}
     >
-      <h1 onClick={() => setShow(!show)}>{title}</h1>
+      <h2 onClick={() => setShow(!show)}>{title}</h2>
       <div style={{ display: show ? "block" : "none" }}>
         {tasks.map((t) => (
-          <TaskItem key={t.id} name={t.name} />
+          <TaskItem key={t.id} task={t} onTaskUpdated={onTaskUpdated} />
         ))}
       </div>
     </div>
   )
 }
 
-function Tabs() {
+function Tabs({ tasks, onTaskUpdated }: { tasks: Task[]; onTaskUpdated: any }) {
   const [activeTab, setActiveTab] = useState(0)
   const hideIfInactive = (tabIndex: number) =>
     activeTab == tabIndex ? "block" : "none"
@@ -80,12 +95,12 @@ function Tabs() {
   return (
     <>
       <div
+        className="header"
         style={{
           width: "100%",
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-evenly",
-          backgroundColor: headerColor,
         }}
       >
         <h2 style={{ color: activeColor(0) }} onClick={() => setActiveTab(0)}>
@@ -99,6 +114,7 @@ function Tabs() {
         {stores.map((store) => (
           <Section
             title={store}
+            onTaskUpdated={onTaskUpdated}
             tasks={tasks.filter((t) => t.store == store)}
           />
         ))}
@@ -109,6 +125,25 @@ function Tabs() {
 }
 
 function App() {
+  const task1: Task = {
+    id: "1",
+    name: "task1",
+    quantity: "100g",
+    store: "Rayon1",
+    checked: false,
+  }
+  const task2: Task = {
+    id: "2",
+    name: "task2",
+    quantity: "2g",
+    store: "Rayon2",
+    checked: true,
+  }
+  const [tasks, setTasks] = useState([task1, task2])
+  const updateTask = (task: Task) => {
+    setTasks([...tasks.filter((t) => t.id != task.id), task])
+  }
+
   return (
     <div
       style={{
@@ -117,11 +152,11 @@ function App() {
         justifyContent: "flex-start",
         alignItems: "stretch",
         height: "100vh",
-        width: "40vw",
+        width: "30vw",
       }}
     >
-      <Header />
-      <Tabs />
+      <Header tasks={tasks} />
+      <Tabs tasks={tasks} onTaskUpdated={updateTask} />
     </div>
   )
 }
